7cc46a45aafaca90af203cf4076ce8f39b6624f8
B-Rad80 <brro7863@colorado.edu>
2018-12-07 03:16:13 -0700

    copied manage.py

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
