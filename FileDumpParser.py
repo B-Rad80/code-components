266a1c34377c47172e523ef4801491bcc48d4353
B-Rad80 <brro7863@colorado.edu>
2018-12-07 03:20:42 -0700

    moved stuff around

--- FileDumpParser.py
+++ FileDumpParser.py
@@ -1,17 +1,69 @@
 #FileDumpParser
-import os, zipfile
+import csv
+import os, zipfile, sys, io
+import codecs
+class FileDumpParser:
 
-cwd = os.getcwd()
-print(cwd)
-dir_name = 'C:\\SomeDirectory'
-extension = ".zip"
+	def __init__(self):
+		self.og = os.getcwd()
 
-os.chdir(cwd) # change directory from working dir to dir with files
+	def unzip(self, file):
+		cwd = os.path.join(self.og, "ZipFIles")
+		print(cwd)
 
-for item in os.listdir(cwd): # loop through items in dir
-    if item.endswith(extension): # check for ".zip" extension
-        file_name = os.path.abspath(item) # get full path of files
-        zip_ref = zipfile.ZipFile(file_name) # create zipfile object
-        zip_ref.extractall(dir_name) # extract file to dir
-        zip_ref.close() # close file
-        os.remove(file_name) # delete zipped file
+		os.chdir(cwd) # change directory from working dir to dir with files
+
+		if(zipfile.is_zipfile(file)):
+			with zipfile.ZipFile(file,"r") as zip_ref:
+				zip_ref.extractall(cwd)
+				zip_ref.close() # close file
+				os.remove(file) # delete zipped file
+		else:
+			print(file, "is not an existing Zipfile!")
+
+	def parseCSV(self, file):
+		#file = "CVSFiles/"+ file
+		#cwd = os.path.join(self.og, "mysite/CSVFiles")
+		#print(cwd)
+
+		#os.chdir(cwd)
+		addy_List = []
+		noAddyList = []
+		decoded_file = file.read().decode('utf-16')
+		io_string = io.StringIO(decoded_file)
+		line_count=0;
+		for row in csv.reader(io_string, delimiter=',', quotechar='|'):
+			if(row == []):
+				print(addy_List)
+				print("\n\nNO ADDR LIST \n\n\n\n")
+				print(noAddyList)
+				ret = [addy_List, noAddyList]
+				return ret
+			print(line_count)
+			if line_count == 0:
+				print('Column names are {", ".join(row)}')
+			elif row[3] != "Rejected":
+				if(row[4] != ""):
+					tl = [row[0], row[4]]
+					addy_List.append(tl)
+				else:
+					tl = [row[0]]
+					noAddyList.append(tl)
+			line_count += 1
+			
+		print(addy_List)
+		print("\n\nNO ADDR LIST \n\n\n\n")
+		print(noAddyList)
+		ret = [addy_List, noAddyList]
+		return ret
+
+
+#TEST Cases
+#heheheheh "bigdumper" .... much funny
+#
+#
+#bigdumper = FileDumpParser()
+#bigdumper.parseCSV("candidates.csv")
+#bigdumper.unzip("Test1.zip")
+#
+#
